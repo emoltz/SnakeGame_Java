b5daf8cb153d98a69b2369cb89b1fbaf2695ee89
emoltz <es5541@nyu.edu>
2021-12-07 16:50:57 -0500

    removed a todo

--- src/com/NYU_introtojava/Main.java
+++ src/com/NYU_introtojava/Main.java
@@ -2,9 +2,8 @@ package com.NYU_introtojava;
 
 public class Main {
 
-    //TODO thread that can resize the window dynamically
     public static void run(){
-        Window_JFrame beginGame = new Window_JFrame();
+        Window_JFrame b = new Window_JFrame();
     }
 
     public static void main(String[] args) {
