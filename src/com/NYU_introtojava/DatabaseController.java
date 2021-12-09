21ca4f7cf617877af83d53c0671a5bb3a9782e2c
emoltz <es5541@nyu.edu>
2021-12-09 17:11:27 -0500

    added .jar

---
+++ src/com/NYU_introtojava/DatabaseController.java
@@ -0,0 +1,20 @@
+package com.NYU_introtojava;
+
+import org.sqlite.JDBC;
+import java.sql.*;
+
+public class DatabaseController extends JDBC {
+
+    public void connectToDatabase(){
+        Connection connection = null;
+        String URL = "";
+
+        try {
+            connection = DriverManager.getConnection(URL);
+        } catch (SQLException e) {
+            System.out.println("Database Connection Failed");
+        }
+    }
+
+
+}
