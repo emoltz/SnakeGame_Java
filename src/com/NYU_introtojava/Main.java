be8faf4d44813b3d83ae4bbce4306c900edbb749
emoltz <es5541@nyu.edu>
2021-12-12 10:58:28 -0500

    main cleanup

--- src/com/NYU_introtojava/Main.java
+++ src/com/NYU_introtojava/Main.java
@@ -1,18 +1,12 @@
 package com.NYU_introtojava;
 
-import javax.xml.crypto.Data;
-
 public class Main {
-
     public static void run() {
         Window_JFrame b = new Window_JFrame();
     }
 
     public static void main(String[] args) {
-//        Main runnable = new Main();
-//        runnable.run();
         run();
-//        Window_JFrame b = new Window_JFrame();
 
     }
 }
